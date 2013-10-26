e3d999e0928ec29c9dd315c5624a60d7644bb7d0
Fsu0413 <Fsu0413@gmail.com>
2013-10-26 21:48:54 +0800

    change version number(I will change version number every day in the future)

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -130,6 +130,7 @@ void Engine::addTranslationEntry(const char *key, const char *value) {
 
 Engine::~Engine() {
     lua_close(lua); //此条语句会导致闪退（只不过是正在退出的过程中闪退，看不出来），不知什么原因
+    //实测，在载入LUA扩展包时会闪退，不载入LUA包没问题
 #ifdef AUDIO_SUPPORT
     Audio::quit();
 #endif
@@ -569,7 +570,7 @@ SkillCard *Engine::cloneSkillCard(const QString &name) const{
 }
 
 QString Engine::getVersionNumber() const{
-    return "20131003";
+    return "20131026";
 }
 
 QString Engine::getVersion() const{
