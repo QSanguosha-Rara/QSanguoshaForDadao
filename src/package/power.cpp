17371bbe555ba5061de5f7f03b2101a66b86f1bd
Fsu0413 <Fsu0413@gmail.com>
2013-12-23 10:16:50 +0800

    finish power package

--- src/package/power.cpp
+++ src/package/power.cpp
@@ -261,22 +261,29 @@ public:
         response_pattern = "@@cunsi";
     }
 
+    static Card::Suit getCorrespondingSuit(Player::Phase phase){
+        switch (phase){
+            case Player::Start:
+            case Player::Finish:
+                return Card::Spade;
+            case Player::Draw:
+                return Card::Diamond;
+            case Player::Play:
+                return Card::Heart;
+            case Player::Discard:
+                return Card::Club;
+            default:
+                return Card::NoSuit;
+        }
+        return Card::NoSuit;
+    }
+
     virtual bool viewFilter(const Card *to_select) const{
         if (Self->isJilei(to_select))
             return false;
-        switch (static_cast<Player::Phase>(Self->property("cunsi_phase").toInt())){
-            case Player::Start:
-            case Player::Finish:
-                return to_select->getSuit() == Card::Spade;
-            case Player::Draw:
-                return to_select->getSuit() == Card::Diamond;
-            case Player::Play:
-                return to_select->getSuit() == Card::Heart;
-            case Player::Discard:
-                return to_select->getSuit() == Card::Club;
-            default:
-                return false;
-        }
+        Card::Suit corresponding_suit = getCorrespondingSuit(static_cast<Player::Phase>(Self->property("cunsi_phase").toInt()));
+        if (corresponding_suit != Card::NoSuit && corresponding_suit == to_select->getSuit())
+            return true;
         return false;
     }
 
@@ -293,11 +300,32 @@ public:
         events << EventPhaseChanging;
     }
 
+private:
+    static QString getPhaseString(Player::Phase phase){
+        switch (phase){
+            case Player::Start:
+                return "start";
+            case Player::Draw:
+                return "draw";
+            case Player::Play:
+                return "play";
+            case Player::Discard:
+                return "discard";
+            case Player::Finish:
+                return "finish";
+            default:
+                return "";
+        }
+        return "";
+    }
+
+public:
     virtual bool trigger(TriggerEvent triggerEvent, Room *room, ServerPlayer *player, QVariant &data) const{
         PhaseChangeStruct change = data.value<PhaseChangeStruct>();
         if (change.to == Player::Start || change.to == Player::Draw || change.to == Player::Play || change.to == Player::Discard || change.to == Player::Finish){
             room->setPlayerProperty(player, "cunsi_phase", static_cast<int>(change.to));
-            if (room->askForUseCard(player, "@@cunsi", "@cunsi"))
+            if (room->askForUseCard(player, "@@cunsi", 
+                    "@cunsi:::" + Card::Suit2String(CunsiVS::getCorrespondingSuit(change.to)) + ":" + getPhaseString(change.to)))
                 player->skip(change.to);
         }
         return false;
@@ -316,7 +344,7 @@ void DuanxieCard::onEffect(const CardEffectStruct &effect) const{
 
 class Duanxie: public ZeroCardViewAsSkill{
 public:
-    Duanxie(): ZeroCardViewAsSkill("duanxue"){
+    Duanxie(): ZeroCardViewAsSkill("duanxie"){
 
     }
 
@@ -325,7 +353,7 @@ public:
     }
 
     virtual bool isEnabledAtPlay(const Player *player) const{
-        return !player->hasUsed("DuanxueCard");
+        return !player->hasUsed("DuanxieCard");
     }
 };
 
@@ -536,8 +564,6 @@ public:
     }
 };
 
-//摸牌阶段，若你仅有1点体力或没有手牌，你可以放弃摸牌，改为从其他每名角色区域内各获得一张牌。
-
 class Hengzheng: public PhaseChangeSkill{
 public:
     Hengzheng(): PhaseChangeSkill("hengzheng"){
@@ -566,7 +592,7 @@ public:
     }
 };
 
-//临时修改：锁定技，当你处于濒死状态时，你失去技能“暴凌”，增加3点体力上限，回复3点体力，并获得技能“崩坏”。
+//技能暂时没有任何用途
 
 class Baoling: public TriggerSkill{
 public:
@@ -589,10 +615,6 @@ public:
         return false;
     }
 };
-
-//当你使用杀或决斗对目标造成伤害时，你可以防止此伤害，并令其选择一项：
-//1.弃置装备区内的所有牌（至少一张），然后失去1点体力
-//2.你选择的其一项技能无效，直到你下回合开始。
 
 class Chuanxin: public TriggerSkill{
 public:
@@ -672,20 +694,20 @@ public:
     }
 };
 
-//每当一名角色使用【杀】指定你的上家/下家为目标后，你可以令你的上家/下家弃置一张装备区里的牌。 
-
 class Fengshi: public TriggerSkill{
 public:
     Fengshi(): TriggerSkill("fengshi"){
         events << TargetConfirmed;
+        frequency = Compulsory;
     }
 
     virtual bool trigger(TriggerEvent triggerEvent, Room *room, ServerPlayer *player, QVariant &data) const{
         CardUseStruct use = data.value<CardUseStruct>();
         if (use.card != NULL && use.card->isKindOf("Slash") && use.from != NULL){
             foreach(ServerPlayer *p, use.to){
-                if (player->isAdjacentTo(p) && player->hasEquip() && player->askForSkillInvoke(objectName(), QVariant::fromValue(p))){
-                    room->askForCard(p, ".|.|.|equipped!", "@fengshi-discard");
+                if (player->isAdjacentTo(p) && player->canDiscard(p, "e") /*&& player->askForSkillInvoke(objectName(), QVariant::fromValue(p))*/){
+                    int id = room->askForCardChosen(player, p, "e", objectName(), false, Card::MethodDiscard);
+                    room->throwCard(id, p, player);
                 }
             }
         }
@@ -704,7 +726,7 @@ bool WuxinCard::targetFilter(const QList<const Player *> &targets, const Player 
             theslash->setSkillName("wuxin");
             theslash->addSubcard(id);
             theslash->deleteLater();
-            bool can_slash = theslash->isAvailable(Self) && theslash->targetFilter(targets, to_select, Self);
+            bool can_slash = theslash->isAvailable(Self) && theslash->targetFilter(targets, to_select, Self) && !Sanguosha->isProhibited(Self, to_select, theslash);
             if (can_slash)
                 return true;
         }
@@ -892,7 +914,7 @@ public:
             }
 
             if (black_skysoldier.isEmpty())
-                return NULL;
+                return false;
             //data
             if (player->askForSkillInvoke("wuxin", data)){
                 room->fillAG(skysoldier, player, disabled_skysoldier);
@@ -965,6 +987,202 @@ public:
     }
 };
 
+HongfaCard::HongfaCard(){
+    mute = true;
+    m_skillName = "hongfa_slash";
+}
+
+bool HongfaCard::targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const{
+    if (targets.length() == 0)
+        return to_select->hasLordSkill("hongfa") && !to_select->getPile("skysoldier").isEmpty() && to_select != Self;
+    else if (targets.length() >= 1){
+        if (!targets[0]->hasLordSkill("hongfa") || targets[0]->getPile("skysoldier").isEmpty())
+            return false;
+
+        QList<int> skysoldiers = targets[0]->getPile("skysoldier");
+
+        QList<const Player *> fixed_targets = targets;
+        fixed_targets.removeOne(targets[0]);
+
+        foreach (int id, skysoldiers){
+            if (Sanguosha->getCard(id)->isBlack()){
+                Slash *theslash = new Slash(Card::SuitToBeDecided, -1);
+                theslash->setSkillName("hongfa");
+                theslash->addSubcard(id);
+                theslash->deleteLater();
+                bool can_slash = theslash->isAvailable(Self) && theslash->targetFilter(fixed_targets, to_select, Self) && !Sanguosha->isProhibited(Self, to_select, theslash);
+                if (can_slash)
+                    return true;
+            }
+        }
+        return false;
+    }
+    return false;
+}
+
+bool HongfaCard::targetsFeasible(const QList<const Player *> &targets, const Player *Self) const{
+    if (targets.length() >= 2 && targets[0]->hasLordSkill("hongfa") && !targets[0]->getPile("skysoldier").isEmpty()){
+
+        QList<int> skysoldiers = targets[0]->getPile("skysoldier");
+
+        QList<const Player *> fixed_targets = targets;
+        fixed_targets.removeOne(targets[0]);
+
+        foreach (int id, skysoldiers){
+            if (Sanguosha->getCard(id)->isBlack()){
+                Slash *theslash = new Slash(Card::SuitToBeDecided, -1);
+                theslash->setSkillName("hongfa");
+                theslash->addSubcard(id);
+                theslash->deleteLater();
+                bool can_slash = theslash->isAvailable(Self) && theslash->targetsFeasible(fixed_targets, Self);
+                if (can_slash)
+                    return true;
+            }
+        }
+        return false;
+    }
+    return false;
+}
+
+const Card *HongfaCard::validate(CardUseStruct &cardUse) const{
+    if (!cardUse.to.first()->hasLordSkill("hongfa") || cardUse.to.first()->getPile("skysoldier").isEmpty())
+        return NULL;
+    Room *room = cardUse.from->getRoom();
+    ServerPlayer *slasher = cardUse.to.first();
+    cardUse.to.removeOne(slasher);
+    QList<int> skysoldier = slasher->getPile("skysoldier");
+    QList<int> black_skysoldier, disabled_skysoldier;
+    foreach(int id, skysoldier){
+        if (Sanguosha->getCard(id)->isBlack()){
+            Slash *theslash = new Slash(Card::SuitToBeDecided, -1);
+            theslash->setSkillName("hongfa");
+            theslash->addSubcard(id);
+            theslash->deleteLater();
+            bool can_slash = theslash->isAvailable(cardUse.from) && 
+                theslash->targetsFeasible(WuxinCard::ServerPlayerList2PlayerList(cardUse.to), cardUse.from);
+            if (can_slash)
+                black_skysoldier << id;
+            else
+                disabled_skysoldier << id;
+        }
+        else
+            disabled_skysoldier << id;
+    }
+
+    if (black_skysoldier.isEmpty())
+        return NULL;
+
+    room->fillAG(skysoldier, cardUse.from, disabled_skysoldier);
+    int slash_id = room->askForAG(cardUse.from, black_skysoldier, false, "hongfa");
+    room->clearAG(cardUse.from);
+
+    room->showCard(slasher, slash_id);
+    bool can_use = room->askForSkillInvoke(slasher, "hongfa_slash", "letslash:" + cardUse.from->objectName());
+    if (can_use){
+        Slash *slash = new Slash(Card::SuitToBeDecided, -1);
+        slash->addSubcard(slash_id);
+        slash->setSkillName("hongfa");
+        return slash;
+    }
+    return NULL;
+}
+
+class HongfaSlashVS: public ZeroCardViewAsSkill{
+public:
+    HongfaSlashVS(): ZeroCardViewAsSkill("hongfa_slash"){
+        attached_lord_skill = true;
+    }
+
+    virtual const Card *viewAs() const{
+        return new HongfaCard;
+    }
+
+    virtual bool isEnabledAtPlay(const Player *player) const{
+        return Slash::IsAvailable(player);
+    }
+
+    virtual bool isEnabledAtResponse(const Player *player, const QString &pattern) const{
+        return pattern == "slash" && Sanguosha->currentRoomState()->getCurrentCardUseReason() == CardUseStruct::CARD_USE_REASON_RESPONSE_USE;
+    }
+};
+
+class HongfaSlash: public TriggerSkill{
+public:
+    HongfaSlash(): TriggerSkill("hongfa_slash"){
+        view_as_skill = new HongfaSlashVS;
+        events << CardAsked;
+        attached_lord_skill = true;
+    }
+
+    virtual bool trigger(TriggerEvent triggerEvent, Room *room, ServerPlayer *player, QVariant &data) const{
+        QStringList ask = data.toStringList();
+        if (ask.first() == "slash"){
+
+            QList<ServerPlayer *> skysoldiers;
+            foreach(ServerPlayer *p, room->getOtherPlayers(player)){
+                QList<int> skysoldier = p->getPile("skysoldier");
+                QList<int> black_skysoldier, disabled_skysoldier;
+                foreach(int id, skysoldier){
+                    if (Sanguosha->getCard(id)->isBlack())
+                        black_skysoldier << id;
+                    else
+                        disabled_skysoldier << id;
+                }
+
+                if (!black_skysoldier.isEmpty())
+                    skysoldiers << p;
+            }
+            //data
+            if (!skysoldiers.isEmpty()){
+                ServerPlayer *zhangjiao = room->askForPlayerChosen(player, skysoldiers, objectName(), "@hongfa-response", true);
+                if (zhangjiao != NULL){
+                    QList<int> skysoldier = zhangjiao->getPile("skysoldier");
+                    QList<int> black_skysoldier, disabled_skysoldier;
+                    foreach(int id, skysoldier){
+                        if (Sanguosha->getCard(id)->isBlack())
+                            black_skysoldier << id;
+                        else
+                            disabled_skysoldier << id;
+                    }
+
+                    room->fillAG(skysoldier, player, disabled_skysoldier);
+                    int slash_id = room->askForAG(player, black_skysoldier, false, "hongfa");
+                    room->clearAG(player);
+
+                    room->showCard(zhangjiao, slash_id);
+                    bool can_use = room->askForSkillInvoke(zhangjiao, "hongfa_slash", "letslash:" + player->objectName());
+                    if (can_use){
+                        Slash *slash = new Slash(Card::SuitToBeDecided, -1);
+                        slash->addSubcard(slash_id);
+                        slash->setSkillName("hongfa");
+                        room->provide(slash);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+class Hongfa: public TriggerSkill{
+public:
+    Hongfa(): TriggerSkill("hongfa$"){
+        events << GameStart;
+    }
+
+    virtual bool triggerable(const ServerPlayer *target) const{
+        return target != NULL && target->hasLordSkill(objectName()) && target->isAlive();
+    }
+
+    virtual bool trigger(TriggerEvent triggerEvent, Room *room, ServerPlayer *player, QVariant &data) const{
+        foreach(ServerPlayer *p, room->getAllPlayers()){
+            room->attachSkillToPlayer(p, "hongfa_slash");
+        }
+        return false;
+    }
+};
+
 PowerPackage::PowerPackage(): Package("Power"){
 
     General *lidian = new General(this, "lidian", "wei", 3);
@@ -1009,6 +1227,8 @@ PowerPackage::PowerPackage(): Package("Power"){
     related_skills.insertMulti("wuxin", "#wuxin-prevent");
     related_skills.insertMulti("wuxin", "#wuxin-slashresponse");
     zhangjiao->addSkill(new Wendao);
+    zhangjiao->addSkill(new Hongfa);
+    skills << new HongfaSlash;
 
     skills << new Baoling; //for future use
 
--- src/package/power.h
+++ src/package/power.h
@@ -19,19 +19,6 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
-class WuxinCard: public SkillCard{
-    Q_OBJECT
-
-public:
-    Q_INVOKABLE WuxinCard();
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
-    virtual const Card *validate(CardUseStruct &cardUse) const;
-
-private:
-    static QList<const Player *> ServerPlayerList2PlayerList(QList<ServerPlayer *> thelist);
-};
-
 class DuanxieCard: public SkillCard{
     Q_OBJECT
 
@@ -49,12 +36,33 @@ public:
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
+class WuxinCard: public SkillCard{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE WuxinCard();
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+    virtual const Card *validate(CardUseStruct &cardUse) const;
+    static QList<const Player *> ServerPlayerList2PlayerList(QList<ServerPlayer *> thelist);
+};
+
 class WendaoCard: public SkillCard{
     Q_OBJECT
 
 public:
     Q_INVOKABLE WendaoCard();
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
+};
+
+class HongfaCard: public SkillCard{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE HongfaCard();
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+    virtual const Card *validate(CardUseStruct &cardUse) const;
 };
 
 #endif
